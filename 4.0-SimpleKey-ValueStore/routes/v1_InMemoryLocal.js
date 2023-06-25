75abcc72a264ddbabc78952b8614fecd1ff2af52
vijay <astro.vijay@gmail.com>
2023-06-25 16:03:20 +0530

    v1 In Memory store, set,get, delete tested

--- 4.0-SimpleKey-ValueStore/routes/v1_InMemoryLocal.js
+++ 4.0-SimpleKey-ValueStore/routes/v1_InMemoryLocal.js
@@ -9,22 +9,30 @@ let requestHandler = function(req, res, next){
 }
 
 let dataGetHandler = function(req, res, next){
-        const bucketName = req.query.bucketName;
+    const bucketName = req.query.id;
     if (buckets[bucketName]) {
         res.json(buckets[bucketName].data);
     } else {
         res.status(404).send(`No bucket with name ${bucketName} exists.`);
     }
 }
+let dataGetParamHandler = function(req, res, next){
+    const bucketName = req.params.id;
+    if (buckets[bucketName]) {
+        res.json(buckets[bucketName].data);
+    } else {
+        res.status(404).send(`No bucket with name ${bucketName} exists.`);
+    }
+}
+
 // 'v1/MemoryBucket/List'
 let listHandler = function (req, res) {
     res.json(Object.keys(buckets));
   };
 
 // '/MemoryBucket/Bucket'
-
 let buckethandler = function (req, res) {
-    const bucketName = req.query.bucketName;
+    const bucketName = req.query.id;
     if (buckets[bucketName]) {
         res.json(buckets[bucketName]);
     } else {
@@ -34,7 +42,7 @@ let buckethandler = function (req, res) {
 
 // '/MemoryBucket/:bucketName'
 let bucketDeletehandler = function (req, res) {
-    const bucketName = req.params.bucketName;
+    const bucketName = req.params.id;
     if (buckets[bucketName]) {
       delete buckets[bucketName];
       res.sendStatus(204);
@@ -42,18 +50,26 @@ let bucketDeletehandler = function (req, res) {
       res.status(404).send(`No bucket with name ${bucketName} exists.`);
     }
   };
-  // '/MemoryBucket/Set'
+  // '/MemoryBucket/set'
   let bucketSethandler = function (req, res) {
-    const bucketName = req.query.bucketName;
+    const bucketName = req.query.id;
     const data = req.body;
     createOrGetBucket(bucketName).data = data;
     res.json(data);
   };
+
+// '/MemoryBucket/set/id'
+let bucketSetParamhandler = function (req, res) {
+    const bucketName = req.params.id;
+    const data = req.body;
+    createOrGetBucket(bucketName).data = data;
+    res.json(data);
+    };
   
   // '/MemoryBucket/Update'
   let bucketUpdatehandler = function (req, res) {
-    const bucketName = req.query.bucketName;
-    const key = req.query.key;
+    const bucketName = req.query.id;
+    const key = req.query.key;  
     const value = req.query.value;
     const bucket = createOrGetBucket(bucketName);
     bucket.data[key] = value;
@@ -77,11 +93,16 @@ let bucketDeletehandler = function (req, res) {
 
 // Home page for this router
 router.get('/', requestHandler)
+router.get('/data/list', listHandler)
 router.get('/data', dataGetHandler)
+router.get('/data/get/:id', dataGetParamHandler)
+router.post('/data/set', bucketSethandler)
+router.post('/data/set/:id', bucketSetParamhandler)
+router.delete('/data/:id', bucketDeletehandler)
 
 module.exports = {
-	name: "users API",
-	description: "API to access user data",
+	name: "v1 API",
+	description: "API to access in Memory data",
     path: "/v1",
 	router,
 }
